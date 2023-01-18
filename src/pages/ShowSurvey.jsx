aaad7d541f74b11d2fb1e96feb7f3a328323c35d
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2023-01-18 17:35:45 +0100

    updates to allow users to see surveys again

--- src/pages/ShowSurvey.jsx
+++ src/pages/ShowSurvey.jsx
@@ -11,6 +11,7 @@ const ShowSurvey = () => {
     useEffect(() => {
       (async () => {
         const response = await fetch("http://localhost/api/surveys/" + id);
+        
         const survey = await response.json();
         setSurvey(survey);
       })();
@@ -23,7 +24,7 @@ const ShowSurvey = () => {
             <div>
                 <article>
                     <p>Survey ID : {survey.id}</p>
-                    {/* <p>User ID : {survey.userID}</p> */}
+                    <p>User ID : {survey.id_users_submitted_survey}</p>
                     <p>Score : {survey.score}</p>
                     <p>Comment : {survey.comment}</p>
                 </article>
